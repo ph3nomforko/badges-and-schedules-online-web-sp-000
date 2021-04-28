744cb1a6deecb44ac88f87f5bf611e4f575d805a
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 22:55:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |name|
-    "Hello my name is #{badge_maker(name)}."
+  attendees.collect do |name|
+    badge_maker(name)
   end
 end
