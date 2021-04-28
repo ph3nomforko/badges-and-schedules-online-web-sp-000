12c9469093371f39b724c339693304dce131e835
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 23:04:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 def assign_rooms(attendees)
   room = 0
-  attendees.each_with_index do | name |
+  attendees.collect do | name |
     room += 1
     "Hello, #{name}! You'll be assigned to room #{room}!"
   end
