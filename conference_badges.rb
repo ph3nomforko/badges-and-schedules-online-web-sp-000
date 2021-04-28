7e33d4924651c7de418bb50e37243da8f2f14f3b
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 23:07:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creater(attendees).each do |name|
+  batch_badge_creator(attendees).each do |name|
     puts name
   end
   assign_rooms(attendees).each do |room|
